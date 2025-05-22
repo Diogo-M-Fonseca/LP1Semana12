f15b5736572a10e5da49a9213082c9b69f66e969
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-05-23 00:39:10 +0100

    Guarda3.cs completed

---
+++ MyGenericClass/Guarda3.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace MyGenericClass
+{
+
+    public class Guarda3<T>
+    {
+        private T Guarda31;
+        private T Guarda32;
+        private T Guarda33;
+        public Guarda3()
+        {
+            Guarda31 = default;
+            Guarda32 = default;
+            Guarda33 = default;
+        }
+        public T GetItem(int i)
+        {
+            switch (i)
+            {
+                case 0: return Guarda31;
+                case 1: return Guarda32;    
+                case 2: return Guarda33;
+                default: throw new IndexOutOfRangeException();
+            }
+            
+        }
+        public void SetItem(int i, T item)
+        {
+            switch (i)
+            {
+                case 0:
+                    Guarda31 = item;
+                    break;
+                case 1:
+                    Guarda32 = item;
+                    break;
+                case 2:
+                    Guarda33 = item;
+                    break;
+                default:
+                    throw new IndexOutOfRangeException();
+            }
+        }
+    }
+}
