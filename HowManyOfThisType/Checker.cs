3cbc75c542b86a1102a4765cbf98c2a7ac9919e9
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-05-22 23:01:16 +0100

    Checker.cs finished

--- HowManyOfThisType/Checker.cs
+++ HowManyOfThisType/Checker.cs
@@ -1,7 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+
 namespace HowManyOfThisType
 {
     public static class Checker
     {
-        
+        public static int HowManyOfThisType<T>(IEnumerable<object> items)
+        {
+            int num = 0;
+
+            foreach (object item in items)
+            {
+
+                if (item is T)
+                {
+                    num++;
+                }
+            }
+            return num;
+        }
     }
 }
